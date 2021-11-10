aec75626828ea4f2b42e7b7a087dd8ba13d7f302
Jornad <jordan.watkins0922@gmail.com>
2021-11-09 20:28:54 -0500

    Working on generating links

--- commands/activity.js
+++ commands/activity.js
@@ -1,11 +1,16 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Invite } = require('discord.js');
 let link = 'LINK_HERE'
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('activity')
 		.setDescription('Starts an activity!'),
 	async execute(interaction) {
         let game = interaction.options.getString('type').slice(9);
-		await interaction.reply('Click this link to start ' + game.replace('_', ' ') + '! ' + link);
+		let VoiceChannel = 
+		let Invite = await VoiceChannel.activityInvite(game.toLowerCase());
+		let link = 'https://discord.com/invite/' + Invite.Code
+		await interaction.reply('Click this link to start ' + game.replace('_', ' ') + '! ' + link + VoiceChannel);
 	},
 };
