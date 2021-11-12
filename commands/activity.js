0a52c9a6960d622fe72fbd07dc84f780d27a10f8
Jornad <jordan.watkins0922@gmail.com>
2021-11-11 20:49:33 -0500

    working on activity function

--- commands/activity.js
+++ commands/activity.js
@@ -1,6 +1,5 @@
+require("discordjs-activity");
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { Invite } = require('discord.js');
-let link = 'LINK_HERE'
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -8,9 +7,11 @@ module.exports = {
 		.setDescription('Starts an activity!'),
 	async execute(interaction) {
         let game = interaction.options.getString('type').slice(9);
-		let VoiceChannel = 
+		let VoiceChannel = interaction.client.channels.cache.get(interaction.member.voice.channelId);
 		let Invite = await VoiceChannel.activityInvite(game.toLowerCase());
-		let link = 'https://discord.com/invite/' + Invite.Code
-		await interaction.reply('Click this link to start ' + game.replace('_', ' ') + '! ' + link + VoiceChannel);
+		//let link = 'https://discord.com/invite/' + Invite.Code
+		//await interaction.reply('Click this link to start ' + game.replace('_', ' ') + '! ' + link + VoiceChannel);
+
+		console.log(Invite);
 	},
 };
--- deploy-commands.js
+++ deploy-commands.js
@@ -28,3 +28,4 @@ const rest = new REST({ version: '9' }).setToken(token);
 rest.put(Routes.applicationCommands(clientId), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
+	
